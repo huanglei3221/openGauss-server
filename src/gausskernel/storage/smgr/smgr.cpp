1549701657e243c2bc00a48b27859ce79f4a6f33
weixin_43820022 <954966764@qq.com>
2025-11-07 10:43:47 +0800

    1

--- src/gausskernel/storage/smgr/smgr.cpp
+++ src/gausskernel/storage/smgr/smgr.cpp
@@ -608,6 +608,7 @@ void smgrdounlink(SMgrRelation reln, bool isRedo, BlockNumber blockNum)
     HTAB *unlink_rel_hashtbl = g_instance.bgwriter_cxt.unlink_rel_hashtbl;
     DelFileTag *entry = NULL;
     bool found = false;
+    
     if (which == EXRTO_MANAGER && (reln->smgr_rnode.node.spcNode == EXRTO_BLOCK_INFO_SPACE_OID ||
                                    is_standby_read_seg_relnode(reln->smgr_rnode.node))) {
         max_forknum = EXRTO_FORK_NUM;
