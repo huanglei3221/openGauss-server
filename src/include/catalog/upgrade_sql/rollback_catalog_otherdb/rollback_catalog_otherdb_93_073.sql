3da445d7f0e487f0b28150c67db8066a6b87be34
chenxiaobin19 <1025221611@qq.com>
2025-11-22 15:34:20 +0800

    修复6.0.2升级到master回滚后查询referential_constraints视图定义失败的问题

---
+++ src/include/catalog/upgrade_sql/rollback_catalog_otherdb/rollback_catalog_otherdb_93_073.sql
@@ -0,0 +1,60 @@
+SET search_path TO information_schema;
+
+DROP VIEW IF EXISTS referential_constraints CASCADE;
+CREATE OR REPLACE VIEW referential_constraints AS
+    SELECT CAST(pg_catalog.current_database() AS sql_identifier) AS constraint_catalog,
+           CAST(ncon.nspname AS sql_identifier) AS constraint_schema,
+           CAST(con.conname AS sql_identifier) AS constraint_name,
+           CAST(
+             CASE WHEN npkc.nspname IS NULL THEN NULL
+                  ELSE pg_catalog.current_database() END
+             AS sql_identifier) AS unique_constraint_catalog,
+           CAST(npkc.nspname AS sql_identifier) AS unique_constraint_schema,
+           CAST(pkc.conname AS sql_identifier) AS unique_constraint_name,
+
+           CAST(
+             CASE con.confmatchtype WHEN 'f' THEN 'FULL'
+                                    WHEN 'p' THEN 'PARTIAL'
+                                    WHEN 'u' THEN 'NONE' END
+             AS character_data) AS match_option,
+
+           CAST(
+             CASE con.confupdtype WHEN 'c' THEN 'CASCADE'
+                                  WHEN 'n' THEN 'SET NULL'
+                                  WHEN 'd' THEN 'SET DEFAULT'
+                                  WHEN 'r' THEN 'RESTRICT'
+                                  WHEN 'a' THEN 'NO ACTION' END
+             AS character_data) AS update_rule,
+
+           CAST(
+             CASE con.confdeltype WHEN 'c' THEN 'CASCADE'
+                                  WHEN 'n' THEN 'SET NULL'
+                                  WHEN 'd' THEN 'SET DEFAULT'
+                                  WHEN 'r' THEN 'RESTRICT'
+                                  WHEN 'a' THEN 'NO ACTION' END
+             AS character_data) AS delete_rule
+
+    FROM (pg_namespace ncon
+          INNER JOIN pg_constraint con ON ncon.oid = con.connamespace
+          INNER JOIN pg_class c ON con.conrelid = c.oid AND con.contype = 'f')
+         LEFT JOIN pg_depend d1  -- find constraint's dependency on an index
+          ON d1.objid = con.oid AND d1.classid = 'pg_constraint'::regclass
+             AND d1.refclassid = 'pg_class'::regclass AND d1.refobjsubid = 0
+         LEFT JOIN pg_depend d2  -- find pkey/unique constraint for that index
+          ON d2.refclassid = 'pg_constraint'::regclass
+             AND d2.classid = 'pg_class'::regclass
+             AND d2.objid = d1.refobjid AND d2.objsubid = 0
+             AND d2.deptype = 'i'
+         LEFT JOIN pg_constraint pkc ON pkc.oid = d2.refobjid
+            AND pkc.contype IN ('p', 'u')
+            AND pkc.conrelid = con.confrelid
+         LEFT JOIN pg_namespace npkc ON pkc.connamespace = npkc.oid
+
+    WHERE pg_catalog.pg_has_role(c.relowner, 'USAGE')
+          -- SELECT privilege omitted, per SQL standard
+          OR pg_catalog.has_table_privilege(c.oid, 'INSERT, UPDATE, DELETE, TRUNCATE, REFERENCES, TRIGGER')
+          OR pg_catalog.has_any_column_privilege(c.oid, 'INSERT, UPDATE, REFERENCES') ;
+
+GRANT SELECT ON referential_constraints TO PUBLIC;
+
+RESET search_path;
