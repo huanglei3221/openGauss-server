185c46bc8371f0506d8ce90e41c41edff8dc07d8
wangfeihuo <wangfeihuo@huawei.com>
2025-11-22 17:04:03 +0800

    修复使用了not与@>进行搭配结果错误的问题

---
+++ src/include/catalog/upgrade_sql/rollback_catalog_maindb/rollback-post_catalog_maindb_93_075.sql
@@ -0,0 +1,17 @@
+DROP OPERATOR IF EXISTS pg_catalog.@>(jsonb, jsonb) cascade;
+SET LOCAL inplace_upgrade_next_system_object_oids = IUO_GENERAL, 3246;
+CREATE OPERATOR pg_catalog.@>(
+leftarg = jsonb, rightarg = jsonb, procedure = jsonb_contains,
+restrict = contsel, join = contjoinsel
+);
+COMMENT ON OPERATOR pg_catalog.@>(jsonb, jsonb) IS 'jsonb_contains';
+
+
+DROP OPERATOR IF EXISTS pg_catalog.<@(jsonb, jsonb) cascade;
+SET LOCAL inplace_upgrade_next_system_object_oids = IUO_GENERAL, 3250;
+CREATE OPERATOR pg_catalog.<@(
+leftarg = jsonb, rightarg = jsonb, procedure = jsonb_contained,
+negator=operator(pg_catalog.@>),
+restrict = contsel, join = contjoinsel
+);
+COMMENT ON OPERATOR pg_catalog.<@(jsonb, jsonb) IS 'jsonb_contained';
